1505b629a943338ecdb9d3dd74bc2950d32df6e5
HyeonE <mag30102000@gmail.com>
2023-06-11 23:08:25 +0900

    class 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { Component } from "react";
 import "./App.css";
 
-function App() {
-  const name = "리액트";
-  return <div className="react">{name}</div>;
+class App extends Component() {
+  render() {
+    const name = "리액트";
+    return <div className="react">{name}</div>;
+  }
 }
 export default App;
